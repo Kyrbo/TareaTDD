adeea2620442d27536f96c5108aa2b08170a06bc
Kyrbo <jhenderson2019@inf.udec.cl>
2025-04-05 20:43:13 -0300

    Implementacion Stop!

--- ohce.py
+++ ohce.py
@@ -1,5 +1,8 @@
 def ohce(texto):
+    if texto == "Stop!":
+        return "Adios"
     resultado = texto[::-1]
     if texto == resultado:
         return f"{resultado}\n¡Bonita palabra!"
     return resultado
+
