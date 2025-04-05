8ec2a3e46b23bce13a08a54500c3f2189d3a0c9a
Kyrbo <jhenderson2019@inf.udec.cl>
2025-04-05 20:51:46 -0300

    Implementacion saludo segun hora actual y terminal interactuable

--- ohce.py
+++ ohce.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def ohce(texto):
     if texto == "Stop!":
         return "Adios"
@@ -6,3 +8,25 @@ def ohce(texto):
         return f"{resultado}\n¡Bonita palabra!"
     return resultado
 
+def saludar(nombre, hora):
+    if 6 <= hora < 12:
+        return f"¡Buenos días, {nombre}!"
+    elif 12 <= hora < 20:
+        return f"¡Buenas tardes, {nombre}!"
+    else:
+        return f"¡Buenas noches, {nombre}!"
+
+#Terimal interactuable
+
+if __name__ == "__main__":
+    
+    nombre = input("¿Cómo te llamas? ")
+    hora_actual = datetime.now().hour
+    print(saludar(nombre, hora_actual))
+
+    while True:
+        entrada = input("> ")
+        respuesta = ohce(entrada)
+        print(respuesta)
+        if entrada == "Stop!":
+            break
