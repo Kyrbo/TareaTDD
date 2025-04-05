381c0a63f41c8ce690ca1db7fed664eba9cc57e3
Kyrbo <jhenderson2019@inf.udec.cl>
2025-04-05 20:45:28 -0300

    Test saludo segun hora actual

--- test_ohce.py
+++ test_ohce.py
@@ -1,5 +1,5 @@
 from ohce import ohce
-
+from ohce import saludar
 def test_palabra_inversa():
     assert ohce("hola") == "aloh"
     
@@ -8,3 +8,15 @@ def test_palindromo():
     
 def test_detenerse_Stop():
     assert ohce("Stop!") == "Adios"
+
+def test_saludo_dia():
+    assert saludar("Juan", 9) == "¡Buenos días, Juan!"
+
+def test_saludo_tarde():
+    assert saludar("Juan", 15) == "¡Buenas tardes, Juan!"
+
+def test_saludo_noche():
+    assert saludar("Juan", 22) == "¡Buenas noches, Juan!"
+
+def test_saludo_madrugada():
+    assert saludar("Juan", 3) == "¡Buenas noches, Juan!"
